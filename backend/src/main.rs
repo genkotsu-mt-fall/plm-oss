4b4612bbea6f8fcc29589e2cd635eae950b764ce
genkotsu-mt-fall <dm0408a@gmail.com>
2025-04-28 09:56:11 +0900

    feat(api): Add PUT /parts/{id} endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,9 @@
 mod handlers;
 
-use axum::Router;
-use axum::routing::get;
-use handlers::create_part;
-use handlers::get_part;
-use handlers::get_parts;
+use axum::{Router, routing::get};
+use handlers::{create_part, get_part, get_parts, update_part};
 use std::sync::Arc;
-use tokio::net::TcpListener;
-use tokio::sync::Mutex;
+use tokio::{net::TcpListener, sync::Mutex};
 
 async fn health_check() -> &'static str {
     "OK"
@@ -20,7 +16,7 @@ async fn main() {
     let app = Router::new()
         .route("/healthz", get(health_check))
         .route("/parts", get(get_parts).post(create_part))
-        .route("/parts/{id}", get(get_part))
+        .route("/parts/{id}", get(get_part).put(update_part))
         .with_state(shared_part);
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Server running at http://localhost:3000");
